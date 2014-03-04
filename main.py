9c49742acf8b0d6c9fb222a7aede751d4880c1d6
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2014-03-04 11:33:23 -0300

    Basic template for a MIT licensed Flask project

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on Tue Mar  4 11:05:12 2014
+# MIT Licensed. See COPYING.TXT for more information.
+
+from flask import Flask
+
+app = Flask()
+
+@app.route("/")
+def index():
+    return "Turing machine!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
