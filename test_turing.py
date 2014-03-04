7c2679632987d01e5d8836b049acd49fd5d36f8f
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2014-03-04 14:13:37 -0300

    Raw rule generator

--- test_turing.py
+++ test_turing.py
@@ -5,7 +5,9 @@
 """ Turing machine internals testing module """
 
 from __future__ import unicode_literals
-from turing import tokenizer
+from turing import TMSyntaxError, tokenizer, raw_rule_generator
+from pytest import raises, mark
+p = mark.parametrize
 
 class TestTokenizer(object):
 
@@ -36,7 +38,8 @@ class TestTokenizer(object):
     assert list(tokenizer(data)) == expected
 
   def test_no_token_at_all(self): # Empty files have a single token
-    assert list(tokenizer("\n\n\n")) == ["\n"] == list(tokenizer(""))
+    assert list(tokenizer("\n\n\n")) == ["\n"] == list(tokenizer("")) \
+                                               == list(tokenizer("\n \n  \n"))
 
   def test_empty_lines(self):
     data = (
@@ -59,3 +62,52 @@ class TestTokenizer(object):
     ]
     print(list(tokenizer(data)))
     assert list(tokenizer(data)) == expected
+
+
+class TestRawRuleGenerator(object):
+
+  def test_no_rules(self):
+    assert list(raw_rule_generator("\n \n\n")) == [] == \
+           list(raw_rule_generator(""))
+
+  def test_one_rule(self):
+    rgen = raw_rule_generator("q1 1 -> P0 R q2")
+    assert next(rgen) == (["q1", "1"], ["P0", "R", "q2"])
+    with raises(StopIteration):
+        next(rgen)
+
+  def test_half_rule(self):
+    rgen = raw_rule_generator("q1 1")
+    with raises(TMSyntaxError):
+        next(rgen)
+
+  @p("rule", ["q1 1 ->", "-> q0"])
+  def test_one_and_a_half_rule_with_arrow(self, rule):
+    rgen = raw_rule_generator("q0 -> q1\n" + rule)
+    assert next(rgen) == (["q0"], ["q1"])
+    with raises(TMSyntaxError):
+        next(rgen)
+
+  @p("rule", ["  ->", "->", "\n->", "->\n\n", "->\n\nq0 -> q1"])
+  def test_one_and_a_arrow_only_rule(self, rule):
+    rgen = raw_rule_generator("q1 1 -> E q0\n" + rule)
+    assert next(rgen) == (["q1", "1"], ["E", "q0"])
+    with raises(TMSyntaxError):
+        next(rgen)
+
+  def test_five_rules(self):
+    rgen = raw_rule_generator(
+      "q4 0 -> P1 R q3\n"
+      "   1 -> Px q4\n\n"
+      "   x -> P0 L\n\n\n"
+      "        P1 q3\n"
+      "q3 0 -> P1 q4\n"
+      "   1 -> P0 L q3"
+    )
+    assert next(rgen) == (["q4", "0"], ["P1", "R", "q3"])
+    assert next(rgen) == ([" ", "1"], ["Px", "q4"])
+    assert next(rgen) == ([" ", "x"], ["P0", "L", "P1", "q3"])
+    assert next(rgen) == (["q3", "0"], ["P1", "q4"])
+    assert next(rgen) == ([" ", "1"], ["P0", "L", "q3"])
+    with raises(StopIteration):
+        next(rgen)
--- turing.py
+++ turing.py
@@ -6,9 +6,15 @@
 from __future__ import unicode_literals
 import re
 
-__all__ = ["tokenizer"]
+__all__ = ["TMSyntaxError", "tokenizer", "raw_rule_generator"]
+
+class TMSyntaxError(SyntaxError):
+    """ Syntax errors for a Turing machine code (rules description) """
 
 def tokenizer(data):
+    """
+    Lexical tokenizer for raw text data containing Turing machine rules.
+    """
     blocks = []
     for line in data.splitlines():
         if "->" in blocks and "->" in line:
@@ -21,3 +27,32 @@ def tokenizer(data):
     for token in blocks:
         yield token
     yield "\n"
+
+def raw_rule_generator(data):
+    """
+    Generator for pairs (config, action), following
+    <rule> ::= <config> "->" <action> "\n"
+    Also ensures that neither the config nor the action should be empty, raising
+    TMSyntaxError when that happens.
+    """
+    config, action = [], []
+    block = config
+    for token in tokenizer(data):
+        if token == "->":
+            block = action
+        elif token == "\n":
+            if action:
+                if not config:
+                    raise TMSyntaxError("Incomplete rule (missing config)")
+                yield config, action
+                config, action = [], []
+                block = config
+            elif config:
+                raise TMSyntaxError("Incomplete rule (missing action)")
+            elif block is action:
+                raise TMSyntaxError("Incomplete rule")
+        else:
+            block.append(token)
+    if block is action:
+        raise TMSyntaxError("Incomplete rule at the end of file")
+
