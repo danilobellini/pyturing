6ffc477a59da0b833c282a7782e5b1ed2ec36df3
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2014-03-04 22:00:36 -0300

    Now Flask renders the index.html

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # MIT Licensed. See COPYING.TXT for more information.
 """ Main application file """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Turing machine!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
